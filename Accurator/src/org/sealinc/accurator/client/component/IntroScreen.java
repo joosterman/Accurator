ac4001d16d6a6048d70ab9b54e4359a0bd65ed10
Jasper E.G. Oosterman <jasper.oosterman@gmail.com>
2014-01-17 16:05:52 +0100

    Scroll into view welcome page

--- Accurator/src/org/sealinc/accurator/client/component/IntroScreen.java
+++ Accurator/src/org/sealinc/accurator/client/component/IntroScreen.java
@@ -1,29 +1,27 @@
 package org.sealinc.accurator.client.component;
 
 import org.sealinc.accurator.client.Accurator;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 public class IntroScreen extends Composite {
 	interface MyUiBinder extends UiBinder<Widget, IntroScreen> {}
+
 	Accurator accurator;
-	
+
 	@UiField
-	Button btnRegister;
+	Label lblLearnMore;
 
-
-		
 	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
-	
+
 	@Override
-	public void setHeight(String height){
+	public void setHeight(String height) {
 		super.setHeight(height);
 	}
 
@@ -31,9 +29,15 @@ public class IntroScreen extends Composite {
 		initWidget(uiBinder.createAndBindUi(this));
 		accurator = acc;
 	}
-	
-	@UiHandler("btnRegister")
-	void btnRegisterClick(ClickEvent e) {
-		accurator.getManagement().openRegister();
+
+	@UiHandler("lblLearnMore")
+	void lblLearnMoreClick(ClickEvent e) {
+		scrollToBottom();
 	}
+
+	private native void scrollToBottom()/*-{
+		alert("scrolling");
+		$wnd.jQuery("div.bottom").scrollIntoView();
+	}-*/;
+
 }
