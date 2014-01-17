cc6b557e267da3ed3b8d31f42da0a3c54959d427
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2014-01-17 15:53:42 +0100

    Functional buttons.

--- Accurator/src/org/sealinc/accurator/client/component/IntroScreen.java
+++ Accurator/src/org/sealinc/accurator/client/component/IntroScreen.java
@@ -3,22 +3,13 @@ package org.sealinc.accurator.client.component;
 import org.sealinc.accurator.client.Accurator;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
 public class IntroScreen extends Composite {
 	interface MyUiBinder extends UiBinder<Widget, IntroScreen> {}
 	Accurator accurator;
-	
-	@UiField
-	Button btnRegister;
-
-
 		
 	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
 	
@@ -31,9 +22,5 @@ public class IntroScreen extends Composite {
 		initWidget(uiBinder.createAndBindUi(this));
 		accurator = acc;
 	}
-	
-	@UiHandler("btnRegister")
-	void btnRegisterClick(ClickEvent e) {
-		accurator.getManagement().openRegister();
-	}
+
 }
