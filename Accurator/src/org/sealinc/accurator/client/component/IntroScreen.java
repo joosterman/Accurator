e9d3472572df5633673f83432ca25f868a5391bb
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2014-12-28 12:16:52 +0100

    Cleaned intro screen (removed elements not essential to experiment.

--- Accurator/src/org/sealinc/accurator/client/component/IntroScreen.java
+++ Accurator/src/org/sealinc/accurator/client/component/IntroScreen.java
@@ -1,26 +1,21 @@
 package org.sealinc.accurator.client.component;
 
 import org.sealinc.accurator.client.Accurator;
+
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class IntroScreen extends Composite {
 	interface MyUiBinder extends UiBinder<Widget, IntroScreen> {}
 
 	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
-	private Accurator accurator;
 	private final String imagePathPrefix = "./images/background/";
 	private final String[] backgroundImages = { "1.jpg" };
 
-	@UiField
-	InlineLabel lblLearnMore;
 	@UiField
 	Image imgBackground;
 
@@ -31,25 +26,9 @@ public class IntroScreen extends Composite {
 
 	public IntroScreen(Accurator acc) {
 		initWidget(uiBinder.createAndBindUi(this));
-		accurator = acc;
 		int index = (int) Math.round(Math.random() * backgroundImages.length);
 		// correct for zero based index
 		if (index > 0) index--;
 		imgBackground.setUrl(imagePathPrefix + backgroundImages[index]);
 	}
-
-	@UiHandler("lblLearnMore")
-	void lblLearnMoreClick(ClickEvent e) {
-		scrollToBottom();
-	}
-
-	private native void scrollToBottom()/*-{
-		var container = $wnd.jQuery('body');
-		var scrollTo = $wnd.jQuery('#divider');
-
-		container.animate({
-			scrollTop : scrollTo.offset().top - container.offset().top + container.scrollTop()
-		});
-
-	}-*/;
 }
