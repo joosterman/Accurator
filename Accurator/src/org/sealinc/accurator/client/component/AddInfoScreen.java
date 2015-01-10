84b47a0e9a51392e3e8062b0117d64fc05351892
Chris Dijkshoorn <c.r.dijkshoorn@vu.nl>
2015-01-10 17:42:26 +0100

    Added code for reading values of fields additional info.

--- Accurator/src/org/sealinc/accurator/client/component/AddInfoScreen.java
+++ Accurator/src/org/sealinc/accurator/client/component/AddInfoScreen.java
@@ -12,6 +12,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.IntegerBox;
@@ -29,9 +30,14 @@ public class AddInfoScreen extends Composite {
 	@UiField
 	TextBox txtEMail;
 	@UiField
-	IntegerBox intAge;
+	IntegerBox intAge, intMuseumVisits;
 	@UiField
-	RadioButton chkGenderMale, chkGenderFemale;
+	RadioButton chkGenderMale, chkGenderFemale, chkUrban, chkSubUrban, chkRural, 
+				chkYesEmployee, chkNoEmployee, chkYesTagging, chkNoTagging,
+				chkTaggingExpert, chkTaggingIntermediate, chkTaggingNovice;
+	@UiField
+	CheckBox chkSocialFacebook, chkSocialTwitter, chkSocialLinkedIn, chkSocialNone,
+			 chkTagSiteWaIsDa, chkTagSiteSpotvogel, chkTagSiteSteve, chkTagNone;
 	@UiField
 	ListBox lbxCountry, lbxLanguage, lbxEducation, lbxIncome, lbxInternetUsage;
 	@UiField
@@ -40,29 +46,277 @@ public class AddInfoScreen extends Composite {
 	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
 	Accurator accurator;
 	
-	@UiHandler("btnAddInfo")
-	void btnRegisterClick(ClickEvent e) {
-		// get the entered fields
-		String mail = txtEMail.getText();
-		System.out.println(mail);
-	}
+	private static List<String> countries, languages;
 	
 	public AddInfoScreen(Accurator acc) {
 		initWidget(uiBinder.createAndBindUi(this));
 		this.accurator = acc;
+		initFields();
+	}
+	
+	@UiHandler("btnAddInfo")
+	void btnAddInfoClick(ClickEvent e) {
+		// get the entered fields
+		int age = getAge();
+		String gender = getGender();
+		String country = getCountry();
+		String community = getCommunity();
+		String language = getLanguage();
+		String education = getEducation();
+		String income = getIncome();
+		String mail = getMail(); 
+		String socialMedia = getSocialMedia();
+		String internetUsage = getInternetUsage();
+		int museumVisits = getMuseumVisits();
+		String museumEmployee = getMuseumEmployee();
+		String taggingExperience = getTaggingExperience();
+		String taggingLevel = getTaggingLevel();
+		String tagSites = getTagSites();
+		
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(country);
+		System.out.println(community);
+		System.out.println(language);
+		System.out.println(education);
+		System.out.println(income);
+		System.out.println(mail);
+		System.out.println(socialMedia);
+		System.out.println(internetUsage);
+		System.out.println(museumVisits);
+		System.out.println(museumEmployee);
+		System.out.println(taggingExperience);
+		System.out.println(taggingLevel);
+		System.out.println(tagSites);
+	}
+	
+	private int getAge() {
+		int age = -1;
+		if(intAge.getValue() != null) {
+			age = intAge.getValue();
+		}
+		return age;
+	}
+	
+	private String getGender() {
+		String gender = "unknown";
+		if(chkGenderMale.getValue() == true) {
+			gender = "male";
+		} else if(chkGenderFemale.getValue() == true){
+			gender = "female";
+		}
+		return gender;
+	}
+	
+	private String getCountry() {
+		String country = "unknown";
+		int countryIndex = lbxCountry.getSelectedIndex();
+		if(countryIndex != 0) {
+			country = lbxCountry.getItemText(countryIndex);
+		}
+		return country;
+	}
+	
+	private String getCommunity() {
+		String community = "unknown";
+		if(chkUrban.getValue() == true) {
+			community = "male";
+		} else if(chkSubUrban.getValue() == true){
+			community = "sub-urban";
+		} else if(chkRural.getValue() == true){
+			community = "rural";
+		}
+		return community;
+	}
+	
+	private String getLanguage() {
+		String language = "unknown";
+		int languageIndex = lbxLanguage.getSelectedIndex();
+		if(languageIndex != 0) {
+			language = lbxLanguage.getItemText(languageIndex);
+		}
+		return language;
+	}
+	
+	private String getEducation() {
+		String education;
+		int educationIndex = lbxEducation.getSelectedIndex();
+		switch (educationIndex) {
+	        case 1:  education = "primary school";
+	                 break;
+	        case 2:  education = "high school";
+	                 break;
+	        case 3:  education = "college";
+	                 break;
+	        case 4:  education = "bachelor";
+	                 break;
+	        case 5:  education = "master";
+	                 break;
+	        case 6:  education = "doctorate";
+	                 break;
+	        default: education = "unknown";
+	                 break;
+		}
+		return education;
+	}
+	
+	private String getIncome() {
+		String income;
+		int incomeIndex = lbxIncome.getSelectedIndex();
+		switch (incomeIndex) {
+	        case 1:  income = "<20";
+	                 break;
+	        case 2:  income = "20 to 35";
+	                 break;
+	        case 3:  income = "35 to 50";
+	                 break;
+	        case 4:  income = "50 to 75";
+	                 break;
+	        case 5:  income = "75 to 100";
+	                 break;
+	        case 6:  income = "100 to 150";
+	                 break;
+	        case 7:  income = "150 to 200";
+            		 break;
+	        case 8:  income = ">200";
+            		 break;
+	        default: income = "unknown";
+	                 break;
+		}
+		return income;
+	}
+	
+	private String getMail() {
+		String mail;
+		if (txtEMail.getText().isEmpty()) {
+			mail = "unknown";
+		} else {
+			mail = txtEMail.getText();
+		}
+		return mail;
+	}
+	
+	private String getSocialMedia() {
+		String socialMedia = "unknown";
+		if(chkSocialFacebook.getValue() == true) {
+			socialMedia = "facebook";
+		} else if(chkSocialLinkedIn.getValue() == true){
+			if (chkSocialFacebook.getValue() == true) {
+				socialMedia += " linkedin";
+			} else {
+				socialMedia = "linkedin";
+			}
+		} else if(chkSocialTwitter.getValue() == true){
+			if (chkSocialFacebook.getValue() == true || chkSocialLinkedIn.getValue() == true) {
+				socialMedia += " twitter";
+			} else {
+				socialMedia = "twitter";
+			}
+		}
+		if(chkSocialNone.getValue() == true)
+			socialMedia = "none";
+		
+		return socialMedia;
+	}
+	
+	private String getInternetUsage() {
+		String internetUsage;
+		int internetUsageIndex = lbxInternetUsage.getSelectedIndex();
+		switch (internetUsageIndex) {
+	        case 1:  internetUsage = "always";
+	                 break;
+	        case 2:  internetUsage = "once a day";
+	                 break;
+	        case 3:  internetUsage = "3 to 5 times a week";
+	                 break;
+	        case 4:  internetUsage = "1 to 2 times a week";
+	                 break;
+	        case 5:  internetUsage = "less than once a week";
+	                 break;
+	        default: internetUsage = "unknown";
+	                 break;
+		}
+		return internetUsage;
+	}
+	
+	private int getMuseumVisits() {
+		int visits = -1;
+		if(intMuseumVisits.getValue() != null) {
+			visits = intMuseumVisits.getValue();
+		}
+		return visits;
+	}
+	
+	private String getMuseumEmployee() {
+		String museumEmployee = "unknown";
+		if(chkYesEmployee.getValue() == true) {
+			museumEmployee = "true";
+		} else if(chkNoEmployee.getValue() == true){
+			museumEmployee = "false";
+		}
+		return museumEmployee;
+	}
+	
+	private String getTaggingExperience() {
+		String taggingExperience = "unknown";
+		if(chkYesTagging.getValue() == true) {
+			taggingExperience = "true";
+		} else if(chkNoTagging.getValue() == true){
+			taggingExperience = "false";
+		}
+		return taggingExperience;
+	}
+	
+	private String getTaggingLevel() {
+		String taggingLevel = "unknown";
+		if(chkTaggingNovice.getValue() == true) {
+			taggingLevel = "novice";
+		} else if(chkTaggingIntermediate.getValue() == true){
+			taggingLevel = "intermediate";
+		} else if(chkTaggingExpert.getValue() == true){
+			taggingLevel = "expert";
+		}
+		return taggingLevel;
+	}
+	
+	private String getTagSites() {
+		String taggingSites = "unknown";
+		if(chkTagSiteWaIsDa.getValue() == true) {
+			taggingSites = "waisda";
+		} else if(chkTagSiteSpotvogel.getValue() == true){
+			if (chkTagSiteWaIsDa.getValue() == true) {
+				taggingSites += " spotvogel";
+			} else {
+				taggingSites = "spotvogel";
+			}
+		} else if(chkTagSiteSteve.getValue() == true){
+			if (chkTagSiteWaIsDa.getValue() == true || chkTagSiteSpotvogel.getValue() == true) {
+				taggingSites += " steve";
+			} else {
+				taggingSites = "steve";
+			}
+		}
+		if(chkTagNone.getValue() == true)
+			taggingSites = "none";
+		
+		return taggingSites;
+	}
+	
+	private void initFields() {
 		intAge.setMaxLength(3);
+		intMuseumVisits.setMaxLength(3);
 		addCountries();
 		addLanguages();
 		addEducationLevels();
 		addIncomeLevels();
 		addInternetUseLevels();
-//		txtEMail.getElement().setAttribute("type", "email");
 	}
 	
 	private void addCountries() {
 		lbxCountry.addItem("");
 		Utility.adminService.getCountries(new AsyncCallback<List<String>>() {
-			public void onSuccess(List<String> countries) {
+			public void onSuccess(List<String> serverCountries) {
+				countries = serverCountries;
 				for (String country : countries) {
 					lbxCountry.addItem(country);
 				}
@@ -75,7 +329,8 @@ public class AddInfoScreen extends Composite {
 	private void addLanguages() {
 		lbxLanguage.addItem("");
 		Utility.adminService.getLanguages(new AsyncCallback<List<String>>() {
-			public void onSuccess(List<String> languages) {
+			public void onSuccess(List<String> serverLanguages) {
+				languages = serverLanguages;
 				for (String language : languages) {
 					lbxLanguage.addItem(language);
 				}
@@ -87,12 +342,12 @@ public class AddInfoScreen extends Composite {
 	
 	private void addEducationLevels() {
 		String[] educationLevels = {"",
-									Utility.constants.primarySchool(),
-									Utility.constants.highSchool(),
-									Utility.constants.college(),
-									Utility.constants.bachelor(),
-									Utility.constants.master(),
-									Utility.constants.doctorate()};
+				Utility.constants.primarySchool(),
+				Utility.constants.highSchool(),
+				Utility.constants.college(),
+				Utility.constants.bachelor(),
+				Utility.constants.master(),
+				Utility.constants.doctorate()};
 		
 		for(int i=0; i<educationLevels.length; i++) {
 			lbxEducation.addItem(educationLevels[i]);
